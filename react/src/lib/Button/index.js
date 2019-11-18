168604adaa87898fabe153f1110969e198615066
Kevin Hyde <kehyde@cisco.com>
2019-11-18 12:40:08 -0800

    feat(Button): import modularized CSS

--- react/src/lib/Button/index.js
+++ react/src/lib/Button/index.js
@@ -8,22 +8,21 @@ import { UIDConsumer } from 'react-uid';
 import ButtonGroupContext from '../ButtonGroupContext';
 import SelectableContext, { makeKeyboardKey } from '../SelectableContext';
 import mapContextToProps from 'react-context-toolbox/mapContextToProps';
+import '@momentum-ui/core/css/components/button/module.css';
 
 class Button extends React.Component {
   componentDidMount() {
     /* eslint-disable no-console */
     const { ariaLabel, ariaLabelledBy } = this.props;
-    (!ariaLabel && !ariaLabelledBy)
-      &&
+    !ariaLabel &&
+      !ariaLabelledBy &&
       console.warn('[@momentum-ui/react] Button: Accessibility could be improved with ariaLabel');
     /* eslint-enable no-console */
   }
 
   handleKeyDown = (e, eventKey) => {
     const { onClick, parentOnSelect, parentKeyDown } = this.props;
-    if (e.which === 32 || e.which === 13 ||
-        e.charCode === 32 || e.charCode === 13) {
-
+    if (e.which === 32 || e.which === 13 || e.charCode === 32 || e.charCode === 13) {
       parentOnSelect && parentOnSelect(e, { eventKey });
       onClick && onClick(e);
     } else {
@@ -64,38 +63,33 @@ class Button extends React.Component {
       ...props
     } = this.props;
 
-    const otherProps = omit({...props}, [
-      'id',
-      'onClick',
-      'parentOnSelect',
-      'parentKeyDown'
-    ]);
+    const otherProps = omit({ ...props }, ['id', 'onClick', 'parentOnSelect', 'parentKeyDown']);
 
-    const isButtonGroupIcon = isButtonGroup => (
-      isButtonGroup
-        && children
-        && React.Children.toArray(children).reduce((prev, child) =>
-          prev
-            ? prev
-            : child.type && child.type.displayName === 'Icon'
-        , false)
-    );
+    const isButtonGroupIcon = isButtonGroup =>
+      isButtonGroup &&
+      children &&
+      React.Children.toArray(children).reduce(
+        (prev, child) => (prev ? prev : child.type && child.type.displayName === 'Icon'),
+        false
+      );
 
     const getChildren = () => {
-      return children
-        && [
-          loading &&
-          <div key='child-0'>
-            <Loading />
-          </div>,
+      return (
+        children && [
+          loading && (
+            <div key="child-0">
+              <Loading />
+            </div>
+          ),
           <span
-            className='md-button__children'
+            className="md-button__children"
             style={{ opacity: `${loading ? 0 : 1}` }}
-            key='child-1'
+            key="child-1"
           >
             {children}
-          </span>
-        ];
+          </span>,
+        ]
+      );
     };
 
     const getColor = () => {
@@ -111,28 +105,27 @@ class Button extends React.Component {
       const validButtonSize = checkButtonSize();
 
       if (!circle && !validButtonSize) {
-        console.warn('[@momentum-ui/react] Button: selected size is not supported for non-circular button. Size will default to 36');
+        console.warn(
+          '[@momentum-ui/react] Button: selected size is not supported for non-circular button. Size will default to 36'
+        );
 
         return '36';
       } else if (large) {
-        console.warn('[@momentum-ui/react] Button: large prop is deprecated and will be removed. Please use size prop.');
+        console.warn(
+          '[@momentum-ui/react] Button: large prop is deprecated and will be removed. Please use size prop.'
+        );
 
-        return !circle
-          ? '52'
-          : '44';
-
+        return !circle ? '52' : '44';
       } else {
         return size === 'none' ? 'size-none' : size;
       }
       /* eslint-enable no-console */
     };
 
-    const checkButtonSize = () => (
-      ['none', '28', '36', '40', '52', 28, 36, 40, 52].includes(size)
-    );
+    const checkButtonSize = () => ['none', '28', '36', '40', '52', 28, 36, 40, 52].includes(size);
 
     const getTabIndex = ({ isButtonGroup, focus }) => {
-      if(!isButtonGroup) {
+      if (!isButtonGroup) {
         return 0;
       } else if (isButtonGroup && focus) {
         return 0;
@@ -148,7 +141,8 @@ class Button extends React.Component {
 
               contextProps.id = this.props.id || id;
               contextProps.uniqueKey = eventKey || contextProps.id;
-              contextProps.active = active || (context && context.active === contextProps.uniqueKey);
+              contextProps.active =
+                active || (context && context.active === contextProps.uniqueKey);
               contextProps.isButtonGroup = (context && context.isButtonGroup) || false;
               contextProps.focus = context && context.focus === contextProps.uniqueKey;
               contextProps.width = (context && context.width) || null;
@@ -158,62 +152,55 @@ class Button extends React.Component {
             }}
           </ButtonGroupContext.Consumer>
         )}
-        </UIDConsumer>
+      </UIDConsumer>
     );
 
     const keyboardNavKey = makeKeyboardKey(keyboardKey || label || children);
 
-    const createElement = cxtProps => React.createElement(
-      tag,
-      {
-        ref: ref => this.button = ref,
-        className:
-          'md-button' +
-          `${(circle && ` md-button--circle`) || ''}` +
-          `${(isButtonGroupIcon(cxtProps.isButtonGroup) && ` md-button--icon-group`) || ''}` +
-          `${(getSize() && ` md-button--${getSize()}`) || ''}` +
-          `${(expand && ` md-button--expand`) || ''}` +
-          `${(color && ` md-button--${getColor()}`) || ''}` +
-          `${(removeStyle && ' md-button--none') || ''}` +
-          `${(cxtProps.active && !disabled && ` active`) || ''}` +
-          `${(className && ` ${className}`) || ''}`,
-        id: cxtProps.id,
-        'data-md-event-key': cxtProps.uniqueKey,
-        onClick: e => this.handleClick(e, cxtProps.uniqueKey),
-        onKeyDown: e => this.handleKeyDown(e, cxtProps.uniqueKey),
-        style: {
-          style,
-          ...cxtProps.width && { width: cxtProps.width }
+    const createElement = cxtProps =>
+      React.createElement(
+        tag,
+        {
+          ref: ref => (this.button = ref),
+          className:
+            'md-button' +
+            `${(circle && ` md-button--circle`) || ''}` +
+            `${(isButtonGroupIcon(cxtProps.isButtonGroup) && ` md-button--icon-group`) || ''}` +
+            `${(getSize() && ` md-button--${getSize()}`) || ''}` +
+            `${(expand && ` md-button--expand`) || ''}` +
+            `${(color && ` md-button--${getColor()}`) || ''}` +
+            `${(removeStyle && ' md-button--none') || ''}` +
+            `${(cxtProps.active && !disabled && ` active`) || ''}` +
+            `${(className && ` ${className}`) || ''}`,
+          id: cxtProps.id,
+          'data-md-event-key': cxtProps.uniqueKey,
+          onClick: e => this.handleClick(e, cxtProps.uniqueKey),
+          onKeyDown: e => this.handleKeyDown(e, cxtProps.uniqueKey),
+          style: {
+            style,
+            ...(cxtProps.width && { width: cxtProps.width }),
+          },
+          disabled: disabled || loading,
+          alt: ariaLabel || label,
+          href: (tag === 'a' && href) || undefined,
+          type: (tag !== 'a' && type) || '',
+          ...(keyboardNavKey && { 'data-md-keyboard-key': keyboardNavKey }),
+          ...(cxtProps.focus && { 'aria-current': `${cxtProps.focus}` }),
+          ...(ariaLabel ? { 'aria-label': ariaLabel } : { 'aria-labelledby': ariaLabelledBy }),
+          tabIndex: cxtProps.tabIndex,
+          ...(tag && tag !== 'button' && { role: 'button' }),
+          ...otherProps,
         },
-        disabled: disabled || loading,
-        alt: ariaLabel || label,
-        href: (tag === 'a' && href) || undefined,
-        type: tag !== 'a' && type || '',
-        ...keyboardNavKey && { 'data-md-keyboard-key': keyboardNavKey },
-        ...cxtProps.focus && { 'aria-current': `${cxtProps.focus}` },
-        ...ariaLabel
-          ? { 'aria-label': ariaLabel }
-          : { 'aria-labelledby': ariaLabelledBy },
-        tabIndex: cxtProps.tabIndex,
-        ...tag && tag !== 'button' && {role: 'button'},
-        ...otherProps,
-      },
-      getChildren()
-    );
+        getChildren()
+      );
 
-    return (
-      label
-        ?
-        <div
-          className={`md-button__container${containerLarge ? '' : '--small'}`}
-        >
-          {withContext()}
-          <div className='md-button__label'>
-            {label}
-          </div>
-        </div>
-        :
-        withContext()
+    return label ? (
+      <div className={`md-button__container${containerLarge ? '' : '--small'}`}>
+        {withContext()}
+        <div className="md-button__label">{label}</div>
+      </div>
+    ) : (
+      withContext()
     );
   }
 }
@@ -301,8 +288,4 @@ Button.defaultProps = {
 
 Button.displayName = 'Button';
 
-export default mapContextToProps(
-  SelectableContext,
-  context => context,
-  Button
-);
+export default mapContextToProps(SelectableContext, context => context, Button);
