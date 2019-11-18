be0a7acb08b30d75a17bfd688460fc0fa7a89522
Kevin Hyde <kehyde@cisco.com>
2019-11-18 12:33:34 -0800

    feat(accordion): add modularized css import

--- react/src/lib/Accordion/index.js
+++ react/src/lib/Accordion/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import '@momentum-ui/core/css/components/accordion/module.css';
 
 class Accordion extends React.Component {
   static displayName = 'Accordion';
