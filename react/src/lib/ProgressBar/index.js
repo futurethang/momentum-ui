3b3d400302720cd9722a0115c6130f29caa60c64
Kevin Hyde <kehyde@cisco.com>
2019-11-18 17:03:28 -0800

    feat(ProgressBar): import modularized CSS

--- react/src/lib/ProgressBar/index.js
+++ react/src/lib/ProgressBar/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import '@momentum-ui/core/css/components/progress/module.css';
 
 const ProgressBar = props => {
   const { label, min, max, value, dynamic, displayFormat, color } = props;
